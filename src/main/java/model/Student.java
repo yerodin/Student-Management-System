01caf03fe36be25d4b24f8b0d83298ee254e02ec
unknown <Yero_din@hotmail.com>
2015-08-22 10:39:12 -0700

    Initial commit

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,8 +1,17 @@
 package model;
 
-/**
- * Created by Yerodin on 8/21/2015.
- */
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Student {
-    boolean academicStatus;
+    private IntegerProperty id = new SimpleIntegerProperty(this, "id");
+    private StringProperty firstName = new SimpleStringProperty(this, "firstName");
+    private StringProperty middleName = new SimpleStringProperty(this, "middleName");
+    private StringProperty lastName = new SimpleStringProperty(this, "lastName");
+    private StringProperty block = new SimpleStringProperty(this, "block");
+    private StringProperty room = new SimpleStringProperty(this, "lastName");
+    private StringProperty faculty = new SimpleStringProperty(this, "lastName");
 }
